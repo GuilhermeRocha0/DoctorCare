1cf08269143fd523e040a14c8e7d37b4ea2c9462
GuilhermeRocha0 <gui.rocha009@gmail.com>
2022-05-06 19:46:44 -0300

    Day 4

--- js/main.js
+++ js/main.js
@@ -1,20 +1,33 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
 function onScroll() {
-    if (scrollY > 0) navigation.classList.add('scroll')
-    else navigation.classList.remove('scroll')
+   showNavOnScroll()
+   showBackToTopButton()
+}
+
+function showNavOnScroll() {
+   if (scrollY > 0) navigation.classList.add("scroll");
+   else navigation.classList.remove("scroll");
+}
+
+function showBackToTopButton() {
+   if (scrollY > 550) backToTopButton.classList.add("show");
+   else backToTopButton.classList.remove("show");
 }
 
 function openMenu() {
-    document.body.classList.add('menu-expanded')
+   document.body.classList.add("menu-expanded");
 }
 
 function closeMenu() {
-    document.body.classList.remove('menu-expanded')
+   document.body.classList.remove("menu-expanded");
 }
 
 ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 700,
+   origin: "top",
+   distance: "30px",
+   duration: 700,
 }).reveal(`#home, 
     #home img, 
     #home .stats, 
